c5690348dd371c9bcaefdf4dcb42fedd32f7b996
Joey Iny <joeyainy@gmail.com>
2024-10-14 15:14:23 -0700

    Remove legacy code

--- src/CrateTokenV2.sol
+++ src/CrateTokenV2.sol
@@ -8,15 +8,8 @@ import {IUniswapV2Router02} from "./interfaces/IUniswapV2RouterV2.sol";
 import {ICrateV2} from "./interfaces/ICrateV2.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-// The total supply is 117,000 tokens
-// Once the bonding curve has sold out 80,000 tokens, the other 37,000 are put in Uniswap with the total ETH in the contract.
-// The LP tokens are then burned, so no one can pull the liquidity.
 contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
     uint256 private constant MAX_SUPPLY = 117_000e18;
-    uint256 private constant MAX_CURVE_SUPPLY = 80_000e18;
-    uint256 private constant CROWDFUND_THRESHOLD = 60_000e18;
-    uint256 private constant CRATE_FEE_PERCENT = 5e15;
-    uint256 private constant ARTIST_FEE_PERCENT = 5e15;
 
     uint256 public constant CROWDFUND_GOAL = 5000 * 1e6;
 
@@ -25,7 +18,7 @@ contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
     address public protocolFeeDestination;
     address public artistFeeDestination;
 
-    uint256 public tokensInCurve;
+    uint256 public unsoldTokens;
 
     uint256 public protocolFees;
     uint256 public artistFees;
@@ -61,7 +54,7 @@ contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
         uniswapV2Router02 = _uniswapV2Router02;
         usdcToken = _usdcToken;
         phase = Phase.CROWDFUND;
-        tokensInCurve = MAX_CURVE_SUPPLY;
+        unsoldTokens = MAX_SUPPLY;
         songURI = _songURI;
         _approve(address(this), uniswapV2Router02, MAX_SUPPLY);
     }
@@ -94,6 +87,7 @@ contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
         uint256 numTokens = calculateTokenAmount(_usdcAmount);
         require(numTokens > 0, "Cannot buy 0 tokens");
 
+        unsoldTokens -= numTokens;
         amountRaised += _usdcAmount;
         crowdfundTokens[sender] += numTokens; //Keep track of how many tokens this user purchased in the bonding curve
         amountPaid[sender] += _usdcAmount;
@@ -154,76 +148,6 @@ contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
         return 5 * 1e6; //$5 per copy
     }
 
-        uint256 tokensToBuy = estimateMaxPurchase(netValue);
-        if (tokensToBuy == 0) revert Zero();
-        if (tokensToBuy < minTokensReceivable) {
-            revert SlippageToleranceExceeded();
-        }
-        buy(tokensToBuy);
-    }
-
-    function buy(uint256 _amount) public payable nonReentrant {
-        if (phase != Phase.BONDING_CURVE) revert WrongPhase();
-
-        if (_amount > tokensInCurve) revert InsufficientTokens();
-        if (tokensInCurve >= 10 ** decimals()) {
-            if (_amount < 10 ** decimals()) revert MustBuyAtLeastOneToken();
-        } else {
-            _amount = tokensInCurve;
-        }
-
-        address sender = LibMulticaller.sender();
-        uint256 totalPayment;
-        uint256 crateFee;
-        uint256 artistFee;
-
-
-        artistFees += artistFee;
-        if (msg.value < totalPayment) revert InsufficientPayment();
-
-        _transfer(address(this), sender, _amount);
-        emit TokenTrade(sender, _amount, true, totalPayment);
-
-        (bool refundSuccess,) = (sender).call{value: msg.value - totalPayment}("");
-        if (!refundSuccess) revert TransferFailed();
-
-        (bool crateFeePaid,) = protocolFeeDestination.call{value: crateFee}("");
-        if (!crateFeePaid) revert TransferFailed();
-
-        if (tokensInCurve == 0) {
-            phase = Phase.MARKET;
-            _addLiquidity();
-            emit BondingCurveEnded();
-        }
-    }
-
-    function sell(uint256 _amount, uint256 minEtherReceivable) external nonReentrant {
-        address sender = LibMulticaller.sender();
-
-        if (phase != Phase.BONDING_CURVE) revert WrongPhase();
-        if (balanceOf(sender) < _amount + crowdfundTokens[sender]) revert InsufficientTokens();
-        if (_amount < 10 ** decimals()) revert MustSellAtLeastOneToken();
-
-        uint256 price = getSellPrice(_amount);
-        uint256 crateFee = (price * CRATE_FEE_PERCENT) / 1 ether;
-        uint256 artistFee = (price * ARTIST_FEE_PERCENT) / 1 ether;
-        artistFees += artistFee;
-        uint256 netSellerProceeds = price - crateFee - artistFee;
-        if (netSellerProceeds < minEtherReceivable) {
-            revert SlippageToleranceExceeded();
-        }
-
-        tokensInCurve += _amount;
-        _transfer(sender, address(this), _amount);
-        emit TokenTrade(sender, _amount, false, netSellerProceeds);
-
-        (bool netProceedsSent,) = (sender).call{value: netSellerProceeds}("");
-        if (!netProceedsSent) revert TransferFailed();
-
-        (bool crateFeePaid,) = protocolFeeDestination.call{value: crateFee}("");
-        if (!crateFeePaid) revert TransferFailed();
-    }
-
     function withdrawArtistFees() public nonReentrant {
         require(phase != Phase.CROWDFUND, "Cannot withdraw artist fees in the Crowdfund phase.");
         address sender = LibMulticaller.sender();
@@ -238,29 +162,6 @@ contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
 
     /// VIEW ///
 
-    function estimateMaxPurchase(uint256 ethAmount) public view returns (uint256) {
-        uint256 remainingSupply = tokensInCurve;
-        uint256 lower = 0;
-        uint256 upper = remainingSupply;
-        while (lower < upper) {
-            uint256 mid = (lower + upper + 1) / 2;
-            if (getBuyPrice(mid) <= ethAmount) {
-                lower = mid;
-            } else {
-                upper = mid - 1;
-            }
-        }
-        return lower;
-    }
-
-    function getBuyPrice(uint256 amount) public view returns (uint256) {
-        return getPrice(MAX_CURVE_SUPPLY - tokensInCurve, amount);
-    }
-
-    function getSellPrice(uint256 amount) public view returns (uint256) {
-        return getPrice(MAX_CURVE_SUPPLY - tokensInCurve - amount, amount);
-    }
-
     function getPrice(uint256 supply, uint256 amount) private pure returns (uint256) {
         return bondingCurve(supply + amount) - bondingCurve(supply);
     }
@@ -270,30 +171,6 @@ contract CrateTokenV2 is ERC20Upgradeable, ReentrancyGuard, ICrateV2 {
     }
 
     /// INTERNAL ///
-
-    function _addLiquidity() internal {
-        uint256 amountTokenDesired = balanceOf(address(this));
-        uint256 amountETHDesired = address(this).balance - artistFees - 0.3 ether;
-        if (amountTokenDesired == 0 || amountETHDesired == 0) revert Zero();
-
-        // Calculate the minimum amounts based on a fair price to prevent front-running
-        uint256 minTokenPrice = getPrice(MAX_CURVE_SUPPLY, 1e18); // Fair market price for 1 token
-        uint256 minTokens = (amountETHDesired * 1e18) / minTokenPrice; // Minimum tokens based on fair price
-
-        (uint256 amountToken, uint256 amountETH, uint256 liquidity) = IUniswapV2Router02(uniswapV2Router02)
-            .addLiquidityETH{value: address(this).balance - artistFees - 0.3 ether}(
-            address(this),
-            amountTokenDesired, // amountTokenDesired
-            minTokens, // amountTokenMin
-            amountETHDesired, // amountETHMin
-            address(0), //where to send LP tokens
-            block.timestamp // Deadline (current time)
-        );
-        if (amountToken == 0 || amountETH == 0 || liquidity == 0) revert Zero();
-        (bool protocolFeePaid,) = protocolFeeDestination.call{value: 0.3 ether}("");
-        if (!protocolFeePaid) revert TransferFailed();
-        emit LiquidityAdded(amountToken, amountETH, liquidity);
-    }
 
     function _update(address from, address to, uint256 value) internal override {
         //tokens can be burned if crowdfund is canceled
